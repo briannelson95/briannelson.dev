8ee1badaac762906a0116fbc37a0959b098f4a04
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-01-13 10:25:19 -0500

    sanity build

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const STUDIO_REWRITE = {
   destination:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3333/admin/:path*"
-      : "/admin/:path*",
+      : "/admin/index.html",
 }
 
 const nextConfig = {
