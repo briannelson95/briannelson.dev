7b4f36a316d84205759b3a3f6c163578b07d013f
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-01-17 19:53:29 -0500

    fixed style

--- components/StudioNavbar.tsx
+++ components/StudioNavbar.tsx
@@ -4,9 +4,9 @@ import {LaunchIcon} from '@sanity/icons'
 export default function StudioNavbar(props: any) {
     return (
         <div>
-            <div className="flex items-center justify-between p-5">
+            <div className="dark:bg-[#1a1a1a] dark:text-[#ACACAC] text-black flex items-center justify-between p-5">
                 <Link href={'/'} className="flex items-center">
-                    <LaunchIcon className="h-6 w-6 mr-2" />
+                    <LaunchIcon className="dark:bg-[#1a1a1a] dark:text-[#ACACAC] text-black h-6 w-6 mr-2" />
                     Go to Website
                 </Link>
             </div>
