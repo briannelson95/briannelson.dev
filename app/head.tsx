4929463b67cc2aac4c670335dda4ce359ac75f64
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-01-21 16:47:03 -0500

    added social preview image

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <title>BrianNelson.dev</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      <meta property="og:image" content="/social-preview.webp" />
     </>
   )
 }
