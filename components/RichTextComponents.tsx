63e060c22565d81713a8a4c68874dc2d9fe6e6d6
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-01-21 11:30:53 -0500

    fixed portable text serializer

--- components/RichTextComponents.tsx
+++ components/RichTextComponents.tsx
@@ -26,18 +26,11 @@ export const RichTextComponent: any = {
         },
     },
     block: {
-        h1: ({ children }: any) => {
-            <h1 className='text-5xl py-10 font-bold'>{children}</h1>
-        },
-        h2: ({ children }: any) => {
-            <h2 className='text-4xl py-10 font-bold'>{children}</h2>
-        },
-        h3: ({ children }: any) => {
-            <h3 className='text-3xl py-10 font-bold'>{children}</h3>
-        },
-        h4: ({ children }: any) => {
-            <h4 className='text-2xl py-10 font-bold'>{children}</h4>
-        },
+        // Ex. 1: customizing common block types
+        h1: ({children}: any) => <h1 className="text-2xl">{children}</h1>,
+        h2: ({ children }: any) => <h2 className='text-4xl py-10 font-bold'>{children}</h2>,
+        h3: ({ children }: any) => <h3 className='text-3xl py-10 font-bold'>{children}</h3>,
+        h4: ({ children }: any) => <h4 className='text-2xl py-10 font-bold'>{children}</h4>,
         blockquote: ({ children }: any) => (
             <blockquote className='border-l-slate-500 border-l-4 pl-5 py-5 my-5'>
                 {children}
