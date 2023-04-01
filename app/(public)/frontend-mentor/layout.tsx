5b8962c5a598de90d43e1fe13b08d05f319c6e72
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-03-31 20:29:45 -0400

    added dynamic navbar for frontend mentor

--- app/(public)/frontend-mentor/layout.tsx
+++ app/(public)/frontend-mentor/layout.tsx
@@ -1,15 +1,20 @@
 import FENav from "@/components/FENav"
+import { client } from "@/lib/sanity.client"
 import "@/styles/globals.css"
+import { groq } from "next-sanity"
 
-export default function RootLayout({
+export const revalidate = 86400;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const data = await client.fetch(groq`*[_type == "frontend"] | order(_createdAt asc) {title, slug}`)
   return (
     <html>
-      <body className='max-w-7xl mx-auto bg-white'>
-        <FENav />
+      <body className=''>
+        <FENav props={data} />
         {children}
       </body>
     </html>
--- components/FENav.tsx
+++ components/FENav.tsx
@@ -4,22 +4,37 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import {MenuIcon} from '@sanity/icons'
 
-export default function FENav() {
+type Navigation = {
+    title: string;
+    slug: Slug;
+}
+
+type Slug = {
+    "_type": 'slug';
+    "current": string;
+}
+
+type Props = Navigation[]
+
+export default function FENav({props}: any) {
     const [open, setOpen] = useState(false);
 
     return (
         <nav className='absolute'>
-            {open 
-                ? <ul className='flex flex-row justify-around'>
-                    <li>
-                        <Link href={'/frontend-mentor'}>Frontend Mentor</Link>
-                    </li>
-                    <li>
-                        <Link href={'frontend-mentor/qr-code-component'}>QR Code</Link>
-                    </li>
-                </ul>
-                : <MenuIcon height={20} width={20} onClick={() => setOpen(!open)} />
-            }
+            <div className='relative'>
+                <MenuIcon className='z-50 absolute bg-black rounded-full text-white m-2' height={40} width={40} onClick={() => setOpen(!open)} />
+                <div className={`${open ? 'bg-slate-500 z-0 w-full rounded-br-lg' : 'hidden'} `}>
+                    <h2 className='text-2xl font-semibold pt-3 pl-16'><Link href='../frontend-mentor' onClick={() => setOpen(!open)}>Frontend Mentor</Link></h2>
+                    <ul className='pt-4 px-4 pb-2'>
+                        {props.map((item: { slug: { current: any }; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined }, index: React.Key | null | undefined) => (
+                            <li key={index} className='w-full bg-slate-400 my-2 px-4 py-2 rounded-md font-medium hover:bg-slate-300 transition-all duration-200 ease-in-out'>
+                                <Link href={`../frontend-mentor/${item.slug.current}`} onClick={() => setOpen(!open)}>{item.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                
+            </div>
         </nav>
     )
 }
