68a8731ae1f5bdc0b0acd1d9dd9fe20a9e67c2f5
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-04-03 12:16:45 -0400

    added icon

--- components/fecomps/Product.tsx
+++ components/fecomps/Product.tsx
@@ -48,7 +48,10 @@ export default function Product({category, title, desc, price, oldPrice, image, 
                     </div>
                     : <h2 className={`${fraunces.className} font-bold text-3xl text-product-dark-cyan pb-6 sm:pb-7`}>{oldPrice ? `$${oldPrice}` : "$99.99"}</h2>
                 }
-                <button className='bg-product-dark-cyan text-white py-4 w-full flex justify-center rounded-lg hover:bg-[#1a4130] active:bg-[#1a4130] transition-all duration-200 ease-in-out' onClick={() => router.push('../frontend-mentor')}>Add to Cart</button>
+                <button className='bg-product-dark-cyan text-white py-4 w-full flex justify-center items-center rounded-lg hover:bg-[#1a4130] active:bg-[#1a4130] transition-all duration-200 ease-in-out' onClick={() => router.push('../frontend-mentor')}>
+                        <svg width="15" height="16" className='mr-2' xmlns="http://www.w3.org/2000/svg"><path d="M14.383 10.388a2.397 2.397 0 0 0-1.518-2.222l1.494-5.593a.8.8 0 0 0-.144-.695.8.8 0 0 0-.631-.28H2.637L2.373.591A.8.8 0 0 0 1.598 0H0v1.598h.983l1.982 7.4a.8.8 0 0 0 .799.59h8.222a.8.8 0 0 1 0 1.599H1.598a.8.8 0 1 0 0 1.598h.943a2.397 2.397 0 1 0 4.507 0h1.885a2.397 2.397 0 1 0 4.331-.376 2.397 2.397 0 0 0 1.12-2.021ZM11.26 7.99H4.395L3.068 3.196h9.477L11.26 7.991Zm-6.465 6.392a.8.8 0 1 1 0-1.598.8.8 0 0 1 0 1.598Zm6.393 0a.8.8 0 1 1 0-1.598.8.8 0 0 1 0 1.598Z" fill="#FFF"/></svg>
+                        Add to Cart
+                </button>
             </div>
         </div>
     )
