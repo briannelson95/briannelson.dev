b3b4441e95581f12b9b75fd9314a7525bbf847a0
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-04-29 10:41:47 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import ColoredText from "./ColoredText";
 export default function Header() {
     return (
         <div className="mb-10">
-            <Link href='/'><h1 className="text-6xl font-extrabold text-center md:mt-20">Brian Nelson</h1></Link>
+            <Link href='/'><h1 className="text-6xl font-extrabold text-center px-4 md:mt-20">Brian Nelson</h1></Link>
             <h3 className="text-4xl font-extrabold text-center capitalize mt-6 dark:text-white">
                 Videographer Turned <ColoredText> Web Developer</ColoredText>
             </h3>
