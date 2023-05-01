292225a6530d0e8b044dbcc70f328f49843ae131
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-05-01 08:15:53 -0400

    Update theme.ts

--- theme.ts
+++ theme.ts
@@ -3,7 +3,7 @@ import { buildLegacyTheme, defineConfig } from 'sanity';
 const props = {
     "--my-white": "#fff",
     "--my-black": "#1a1a1a",
-    "--brand": "#F7AB0A",
+    "--brand": "#a855f7",
     "--my-red": "#db4437",
     "--my-yellow": "#f4b400",
     "--my-green": "#0f9d58",
