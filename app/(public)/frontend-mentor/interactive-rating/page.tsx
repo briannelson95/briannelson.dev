ca3698e2b39f9e795a9140c53e6aeccae2905402
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-07-30 08:26:19 -0400

    metadata

--- app/(public)/frontend-mentor/interactive-rating/page.tsx
+++ app/(public)/frontend-mentor/interactive-rating/page.tsx
@@ -1,6 +1,10 @@
 import RatingCard from '@/components/fecomps/RatingCard'
 import React from 'react'
 
+export const metadata = {
+    title: "Interacrive Rating | Frontend Mentor"
+}
+
 export default function page() {
     return (
         <main className='bg-rating-very-dark-blue text-rating-white w-full min-h-screen flex justify-center items-center'>
